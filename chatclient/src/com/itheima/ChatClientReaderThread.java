9d5c720e9252260ece18974aaa8c571394332bb1
loli0con <loli0con@163.com>
2021-06-22 03:29:01 +0800

    很粗暴的日志，没有任何意义

--- chatclient/src/com/itheima/ChatClientReaderThread.java
+++ chatclient/src/com/itheima/ChatClientReaderThread.java
@@ -1,6 +1,8 @@
 package com.itheima;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -13,19 +15,15 @@ public class ChatClientReaderThread extends Thread {
 
     @Override
     public void run() {
-        try {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter("client_log.txt", true))) {
             // 得到socket的字节输入流,包装成字符流,包装成字符输入缓冲流,方便后续以行为单位读取
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String msg;
             while ((msg = br.readLine()) != null) {
                 System.out.println("你收到消息: " + msg);
+                out.write(msg + "\n");
             }
             socket.shutdownInput();
-//            if (!socket.isOutputShutdown()) {
-//                socket.getOutputStream().flush();
-//                socket.shutdownOutput();
-//            }
-//            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
